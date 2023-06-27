57ec29824681c8d1eebb604793142b89cbd029db
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2023-06-27 18:22:52 +0100

    Update script.js

--- rave/script.js
+++ rave/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const shapesContainer = document.getElementById('shapes-container');
   const numberOfShapes = 30;
+  const audio = document.getElementById('audio');
 
   for (let i = 0; i < numberOfShapes; i++) {
     const shape = document.createElement('div');
@@ -8,4 +9,13 @@ window.addEventListener('DOMContentLoaded', () => {
     shape.style.animationDelay = `${i * 0.1}s`;
     shapesContainer.appendChild(shape);
   }
+
+  // Play/pause audio on click
+  document.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  });
 });
