d78703d5307a5930914d90b9ab4113ab9e629302
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2023-06-27 19:02:18 +0100

    Update script.js

--- rave/script.js
+++ rave/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
   const shapesContainer = document.getElementById('shapes-container');
-  const lightningContainer = document.createElement('div');
-  lightningContainer.id = 'lightning-container';
-  document.body.appendChild(lightningContainer);
+  const raveContainer = document.getElementById('rave-container');
+  const raveText = document.getElementById('rave');
+  const shootingStar = document.getElementById('shooting-star');
   const numberOfShapes = 30;
   const audio = document.getElementById('audio');
 
@@ -24,17 +24,48 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Trigger lightning flash
-  setInterval(() => {
-    if (Math.random() < 0.2) {
-      const lightningFlash = document.createElement('div');
-      lightningFlash.classList.add('lightning');
-      lightningContainer.appendChild(lightningFlash);
+  // Trigger shooting star
+  setTimeout(() => {
+    raveText.style.opacity = '1';
+    shootingStar.style.opacity = '1';
+    shootingStar.addEventListener('animationend', () => {
+      raveText.style.opacity = '0';
+      animateRaveText();
+    });
+  }, 2000);
+
+  function animateRaveText() {
+    const letters = raveText.innerText.split('');
+    raveText.innerHTML = '';
+
+    letters.forEach(letter => {
+      const span = document.createElement('span');
+      span.innerText = letter;
+      raveText.appendChild(span);
+    });
+
+    const spanLetters = raveText.querySelectorAll('span');
+
+    spanLetters.forEach((span, index) => {
+      const delay = Math.random() * 2 + 1; // Random delay between 1 and 3 seconds
+      const x = Math.random() * 100 - 50; // Random x position between -50 and 50
+      const y = Math.random() * 100 - 50; // Random y position between -50 and 50
+
+      span.style.transition = `transform ${delay}s`;
+      span.style.transform = `translate(${x}vw, ${y}vh)`;
+      span.style.opacity = '0';
+
       setTimeout(() => {
-        lightningContainer.removeChild(lightningFlash);
-      }, 300);
-    }
-  }, 3000);
+        span.style.transition = 'none';
+        span.style.transform = 'translate(0, 0)';
+        span.style.opacity = '1';
+      }, delay * 1000);
+    });
+
+    setTimeout(() => {
+      raveText.innerHTML = raveText.innerText;
+    }, (letters.length + 1) * 1000); // Delay before rejoining the letters
+  }
 
   function getRandomPosition() {
     return Math.random() * 80; // Adjust the range as needed
